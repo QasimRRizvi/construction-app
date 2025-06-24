8e2ea8d609ac772294b3c806f3de35a513e32f2c
QasimRRizvi <syed.rizvi2385@gmail.com>
2025-06-24 19:55:23 +0500

    feat: add basic auth hook using Zustand

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+type User = {
+    id: string;
+    name: string;
+}
+
+type AuthUser = {
+    user: User | null;
+    login: (user: User) => void;
+    logout: () => void;
+}
+
+export const userAuth = create<AuthUser>((set) => ({
+    user: null,
+    login: (user) => set({ user }),
+    logout: () => set({ user: null })
+}))
